be579b57547a26fbba90d00b0be573440d2addd6
编号 <109009971+bbpn-cn@users.noreply.github.com>
2025-03-25 04:25:00 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import asyncio
 # 接口地址
 API_URL = "https://v.api.aa1.cn/api/60s-v3/"
 
-@register("今日新闻", "Your Name", "获取60秒读懂世界图片的插件", "1.0.0", "repo url")
+@register("今日新闻", "egg", "60秒国内新闻", "1.0.0", "https://github.com/bbpn-cn/headline")
 class NewsPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -29,13 +29,13 @@ class NewsPlugin(Star):
     @filter.command("今日新闻")
     async def news_command(self, event: AstrMessageEvent):
         '''新闻查询指令，使用格式：/今日新闻'''
-        yield event.plain_result("正在获取今日新闻图片...")
+        yield event.plain_result("正在获取今日新闻...")
 
         async with aiohttp.ClientSession() as session:
             if await self.verify_image(session):
                 yield event.image_result(API_URL)  # 直接传入 URL
             else:
-                yield event.plain_result("无法获取今日新闻图片，请稍后再试。")
+                yield event.plain_result("无法获取今日新闻，请稍后再试。")
 
     async def terminate(self):
         '''插件卸载时调用'''
